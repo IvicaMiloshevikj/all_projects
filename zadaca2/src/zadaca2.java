64e7e3ed7402d8730958c244f4b2be5b305d26b5
IvicaMiloshevikj <147504047+IvicaMiloshevikj@users.noreply.github.com>
2023-12-19 12:37:18 +0100

    Add files via upload

---
+++ zadaca2/src/zadaca2.java
@@ -0,0 +1,17 @@
+public class zadaca2 {
+    public static void main(String[] args) {
+        int a = 5; // Vnesete vrednost na a
+        int b = 10; // Vnesete vrednost na b
+        int c = 3; // Vnesete vrednost na c
+        int k;
+        int d;
+
+        if (a <= 5 && b > (a - c)) {
+            k = 1;
+            System.out.println("k = 1");
+        } else {
+            d = 0;
+            System.out.println("d = 0");
+        }
+    }
+}
